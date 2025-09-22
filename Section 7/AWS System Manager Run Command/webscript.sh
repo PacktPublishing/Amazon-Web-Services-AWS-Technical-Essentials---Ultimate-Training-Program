88e0d8742737c45cfcc4f7794361f342dfb10d6d
VrushaliPKelkar <157000009+VrushaliPKelkar@users.noreply.github.com>
2025-09-22 23:43:27 +0530

    Code Files udpated.

--- Section 7/AWS System Manager Run Command/webscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum -y update
-yum install -y httpd
-yum install -y ruby
-cd /home/ec2-user
-curl -O https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
